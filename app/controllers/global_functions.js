b24951d6f21eb243fce75b3e83288d30bf4075a7
Gerardo <g.socias29@gmail.com>
2023-01-12 10:23:39 +0800

    updates

--- app/controllers/global_functions.js
+++ app/controllers/global_functions.js
@@ -1,7 +1,7 @@
 const sql = require("../models/db.js");
 
 exports.getUserTopicsById = (request, result) => {
-  sql.query('SELECT ut.*, utq.quiz_id, utq.score FROM `user_topics` as ut LEFT JOIN user_topics_quiz as utq ON ut.topic_id = utq.user_topic_id WHERE ut.user_id = '+request.params.user_id+';', (err, res) => {
+  sql.query('SELECT ut.*, utq.quiz_id, utq.score FROM `user_topics` as ut LEFT JOIN user_topics_quiz as utq ON ut.id = utq.user_topic_id WHERE ut.user_id = '+request.params.user_id+';', (err, res) => {
     if (err) {
       return result.status(500).send({
         message: err.message || "Some error occurred while retrieving data."
