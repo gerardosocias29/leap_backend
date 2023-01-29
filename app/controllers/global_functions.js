a91dde5e62b18d426af2a6c052480e98cfb41b09
Gerardo <g.socias29@gmail.com>
2023-01-29 22:19:04 +0800

    updates

--- app/controllers/global_functions.js
+++ app/controllers/global_functions.js
@@ -124,3 +124,151 @@ exports.getLessonsByChapterId = (request, result) => {
     return result.send(res);
   });
 }
+
+exports.getAchivementsList = (request, result) => {
+  sql.query(`
+    SELECT * FROM achievements `, async (err, achievements) => {  if (err) { return result.status(500).send({ message: err.message || "Some error occurred while retrieving data." }); }
+      return result.send(achievements);
+    }
+  )
+}
+
+exports.calculateAchievementsFinishedLessons = (req, result) => {
+  // Validate request
+  if (req.headers['content-type'] === 'application/json;') {
+    req.headers['content-type'] = 'application/json';
+  }
+
+  if (!req.body || req.body == "") {
+  return res.status(400).send({
+    message: "Content can not be empty!"
+  });
+  }
+  var params = req.body;
+
+  var achievement_id = params.achievement_id;
+  var achievement_score = params.achievement_achievement_score;
+  var achievement_chapter_ids = params.achievement_chapter_ids;
+  var user_id = params.user_id;
+
+  let query = `SELECT
+    c.id,
+    l.lesson_name,
+      (SELECT COUNT(*) FROM topics t WHERE t.lesson_id = l.id) as topic_count,
+      IFNULL( 
+      (tb1.topic_done
+        / 
+      (SELECT COUNT(*) FROM topics t WHERE t.lesson_id = l.id)) * 100 
+      ,0) as percentage,
+        IFNULL( tb1.topic_done, 0 ) as topic_done
+    FROM lessons l
+    LEFT JOIN chapters c ON c.id = l.chapter_id
+    LEFT JOIN (SELECT COUNT(ut.id) as topic_done, t.lesson_id
+      FROM user_topics ut
+      LEFT JOIN topics t ON t.id = ut.topic_id
+      WHERE ut.user_id = ${user_id}
+        group by ut.id
+    ) as tb1 ON tb1.lesson_id = l.id
+
+    WHERE c.id IN (${achievement_chapter_ids})
+    ORDER BY tb1.topic_done DESC
+    ${ achievement_score == 5 ? 'LIMIT 5' : ''}
+  `;
+  sql.query(query, (err, res) => {
+    if (err) {
+      return result.status(500).send({
+        message: err.message || "Some error occurred while retrieving data."
+      });
+    }
+    var percentage = 0.0;
+    res.forEach((value, index) => {
+      percentage += parseFloat(value.percentage);
+    });
+    percentage = parseFloat(percentage / res.length).toFixed(2);
+    var notif = (percentage == 100) ? 'notify_ready' : 'notify_not_ready';
+    var status = (notif == 'notify_ready') ? ', status = "' + notif +'"' : ', status = "' + notif +'"';
+    sql.query(`
+      INSERT INTO user_achievements (user_id, achievement_id, status, progress) VALUES (${user_id},${achievement_id},'${notif}',${percentage})
+      ON DUPLICATE KEY UPDATE progress = ${percentage} ${status}
+    `, (err, ua) => {
+      if (err) {
+        console.log("error: ", err);
+        return;
+      }
+      return;
+    });
+    return result.send({status: 'success'});
+  });
+  
+}
+
+exports.calculateAchievementsAllQuizzes = (req, result) => {
+  if (req.headers['content-type'] === 'application/json;') {
+    req.headers['content-type'] = 'application/json';
+  }
+
+  if (!req.body || req.body == "") {
+  return res.status(400).send({
+    message: "Content can not be empty!"
+  });
+  }
+  var params = req.body;
+  var achievement_id = params.achievement_id;
+  var achievement_score = params.achievement_achievement_score;
+  var achievement_chapter_ids = params.achievement_chapter_ids;
+  var user_id = params.user_id;
+
+  let query = `
+    SELECT 
+      c.chapter_name,
+      COUNT(tb1.quiz_done) as quiz_done,
+      (SELECT COUNT(q.id) 
+      FROM quizzes q 
+      LEFT JOIN topics t ON t.id = q.topic_id 
+      LEFT JOIN lessons les ON t.lesson_id = les.id
+      WHERE les.chapter_id = c.id
+      ) as quiz_count
+    FROM chapters c
+
+    LEFT JOIN lessons l ON l.chapter_id = c.id
+
+    LEFT JOIN (
+      SELECT utq.id as quiz_done, topics.lesson_id    
+        FROM user_topics_quiz utq
+        LEFT JOIN user_topics ut ON ut.id = utq.user_topic_id
+        LEFT JOIN topics ON topics.id = ut.topic_id
+      WHERE utq.user_id = ${user_id}
+        GROUP BY utq.id
+    ) as tb1 ON tb1.lesson_id = l.id
+
+    WHERE c.id IN (${achievement_chapter_ids})
+    GROUP BY c.chapter_name
+  `;
+
+  sql.query(query, (err, res) => {
+    if (err) {
+      return result.status(500).send({
+        message: err.message || "Some error occurred while retrieving data."
+      });
+    }
+    var percentage = 0.0;
+    res.forEach((value, index) => {
+      percentage += parseFloat((value.quiz_done / value.quiz_count) * 100);
+    });
+    percentage = parseFloat(percentage / res.length).toFixed(2);
+    var new_percentage = percentage >= (achievement_score*1) ? (achievement_score*1) : percentage;
+    var notif = (percentage >= (achievement_score*1)) ? 'notify_ready' : 'notify_not_ready';
+    var status = (notif == 'notify_ready') ? ', status = "' + notif +'"' : ', status = "' + notif +'"';
+    sql.query(`
+      INSERT INTO user_achievements (user_id, achievement_id, status, progress) VALUES (${user_id},${achievement_id},'${notif}',${new_percentage})
+      ON DUPLICATE KEY UPDATE progress = ${new_percentage} ${status}
+    `, (err, ua) => {
+      if (err) {
+        console.log("error: ", err);
+        return;
+      }
+      return;
+    });
+    return result.send({status: 'success'});
+  });
+}
