4488a0a102ef005a024a888b8d7d0e326547a5d2
Gerardo <g.socias29@gmail.com>
2023-05-18 18:38:03 +0800

    updates

--- app/controllers/migration.controller.js
+++ app/controllers/migration.controller.js
@@ -6,8 +6,8 @@ exports.migrate = (res) => {
         'ALTER TABLE `users` CHANGE `course` `course` VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL;',
         'ALTER TABLE `users` ADD `school_user_id` VARCHAR(50) NULL DEFAULT NULL AFTER `deleted_at`;',
         'UPDATE `quizzes` SET `quiz_type` = "1" WHERE quiz_type = "easy"',
-        'UPDATE `quizzes` SET `quiz_type` = "1" WHERE quiz_type = "medium"',
-        'UPDATE `quizzes` SET `quiz_type` = "1" WHERE quiz_type = "hard"',
+        'UPDATE `quizzes` SET `quiz_type` = "2" WHERE quiz_type = "medium"',
+        'UPDATE `quizzes` SET `quiz_type` = "3" WHERE quiz_type = "hard"',
     ];
 
     const migration = new Topic({
